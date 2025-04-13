159024a9c78b2150993a6b81d239b8014d67b69a
Dillon Smart <dillonsmart00@gmail.com>
2025-04-13 09:55:13 +0100

    Added method to find sitemap from common sitemap urls

--- src/scraper.py
+++ src/scraper.py
@@ -2,18 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 from constants import REQUEST_HEADERS, REQUEST_TIMEOUT
 
+sitemap_url = None
+
 
 def fetch_website(url: str):
+    global sitemap_url
     try:
         r = requests.get(url, timeout=REQUEST_TIMEOUT, headers=REQUEST_HEADERS)
         r.raise_for_status()
-        parse_webpage(r)
+        sitemap_url = find_sitemap(url)
+
     except requests.exceptions.RequestException as e:
         print(f"Error fetching {url}: \n {e}")
         return
 
 
-def parse_webpage(r: requests.Response):
-    soup = BeautifulSoup(r.text, "html.parser")
-    page_title = soup.title.string if soup.title else "No title found"
-    print(f"Page title: {page_title}")
+def find_sitemap(url: str):
+    global sitemap_url
+    if sitemap_url is not None:
+        return sitemap_url
+
+    common_sitemaps = [
+        "/sitemap.xml",
+        "/sitemap"
+    ]
+
+    for sitemap in common_sitemaps:
+        sm_url = url + sitemap
+        try:
+            r = requests.get(sm_url, timeout=REQUEST_TIMEOUT, headers=REQUEST_HEADERS)
+            r.raise_for_status()
+            print(f"Sitemap found: {sm_url}")
+            return sm_url
+        except requests.exceptions.RequestException:
+            continue
